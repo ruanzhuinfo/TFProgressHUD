517eac559ea61b9ecf751cfe25c68a7b903429ac
taffy <taffy@zhihu.com>
2016-02-22 13:39:32 +0800

    add Project

---
+++ DDShareProgressHUD.h
@@ -0,0 +1,46 @@
+//
+//  DDShareProgressHUD.h
+//  daily
+//
+//  Created by taffy on 15/12/3.
+//  Copyright © 2015年 Zhihu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ProgressIndicatorView.h"
+
+typedef void(^CompletionBlock)();
+
+@interface DDShareProgressHUD : UIView
+
+@property (nonatomic, strong) UIView *maskView;
+@property (nonatomic, strong) ProgressIndicatorView *progressView;
+
++ (id) share;
+
+/**
+ *  纯文本的提示框
+ *
+ *  @param text       提示语
+ *  @param inView     显示在指定 view 上
+ *  @param delayHide  显示时间
+ *  @param completion 提示框消失后的回调
+ */
+- (void) showWithText: (NSString *)text inView: (UIView *)inView
+            delayHide: (NSTimeInterval)delayHide completion: (CompletionBlock) completion;
+
+/**
+ *  显示操作进度
+ *
+ *  @param inView 显示在指定的 view 上
+ */
+- (void) showProgressAddToView: (UIView *)inView;
+
+/**
+ *  操作完成后要调用的方法
+ *
+ *  @param completion 方法完成后的回调
+ */
+- (void) finishedProgress:(BOOL)hideDuration completion: (CompletionBlock)completion;
+
+@end
---
+++ DDShareProgressHUD.m
@@ -0,0 +1,102 @@
+//
+//  DDShareProgressHUD.m
+//  daily
+//
+//  Created by taffy on 15/12/3.
+//  Copyright © 2015年 Zhihu. All rights reserved.
+//
+
+#import "DDShareProgressHUD.h"
+
+#define WIDTH_SCREEN_RAW ([UIScreen mainScreen].bounds.size.width)
+#define HEIGHT_SCREEN_RAW ([UIScreen mainScreen].bounds.size.height)
+#define WIDTH_SCREEN (MIN(WIDTH_SCREEN_RAW, HEIGHT_SCREEN_RAW))
+#define HEIGHT_SCREEN (MAX(WIDTH_SCREEN_RAW, HEIGHT_SCREEN_RAW))
+
+@implementation DDShareProgressHUD {
+  
+}
+
++ (id) share {
+  static DDShareProgressHUD *HUD;
+  
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    HUD = [DDShareProgressHUD new];
+    HUD.maskView = [[UIView alloc] initWithFrame:CGRectZero];
+    HUD.progressView = [[ProgressIndicatorView alloc] init];
+  });
+  
+  return HUD;
+}
+
+
+- (void) showWithText: (NSString *)text inView: (UIView *)inView
+            delayHide: (NSTimeInterval)delayHide completion: (CompletionBlock) completion {
+  [self.maskView setFrame:inView.bounds];
+  [self.maskView setBackgroundColor:[UIColor clearColor]];
+  [self.maskView setAlpha:0];
+  [inView addSubview:self.maskView];
+  
+  UIView *tipView = [[UIView alloc] initWithFrame:CGRectZero];
+  [tipView setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.90]];
+  [tipView.layer setCornerRadius:5.0];
+  [self.maskView addSubview:tipView];
+  
+  UILabel *tipText = [[UILabel alloc] initWithFrame:CGRectZero];
+  [tipView addSubview:tipText];
+  [tipText setText:text];
+  [tipText setTextColor:[UIColor whiteColor]];
+  [tipText setTextAlignment:NSTextAlignmentCenter];
+  [tipText setFont:[UIFont systemFontOfSize:16]];
+  [tipText sizeToFit];
+  [tipView setFrame:CGRectMake((WIDTH_SCREEN - tipText.bounds.size.width - 20) / 2,
+                               (HEIGHT_SCREEN - tipText.bounds.size.height - 20) / 2,
+                               tipText.bounds.size.width + 20,
+                               tipText.bounds.size.height + 20)];
+  [tipText setFrame:CGRectMake(10, 10, tipText.bounds.size.width, tipText.bounds.size.height)];
+  
+  [UIView animateWithDuration:0.1 animations:^{
+    [self.maskView setAlpha:1.0];
+  } completion:^(BOOL finished) {
+    [UIView animateWithDuration:0.1
+                          delay:1.0
+                        options:UIViewAnimationOptionCurveEaseOut animations:^{
+                          [self.maskView setAlpha:0];
+                        } completion:^(BOOL finished) {
+                          [self.maskView removeFromSuperview];
+                          [tipView removeFromSuperview];
+                          completion();
+                        }];
+  }];
+  
+  
+  
+  
+}
+- (void) showProgressAddToView: (UIView *)inView {
+  [self.maskView setFrame:CGRectMake(0, 64, inView.bounds.size.width, inView.bounds.size.height - 64)];
+  [self.maskView setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.5]];
+  [self.maskView setAlpha:0];
+  [inView addSubview:self.maskView];
+  
+  [UIView animateWithDuration:0.1 animations:^{
+    [self.maskView setAlpha:1.0];
+  }];
+  
+  [self.progressView showProgressAddToView:self.maskView];
+}
+
+
+- (void) finishedProgress:(BOOL)hideDuration completion:(CompletionBlock)completion {
+  
+  [self.progressView finishedProgress:hideDuration completionProgress:^{
+    [UIView animateWithDuration:0.1 animations:^{
+      [self.maskView setAlpha:0];
+    } completion:^(BOOL finished) {
+      completion();
+    }];
+  }];
+}
+
+@end

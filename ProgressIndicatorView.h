8607a41a507d926929d097515a788a9d5c76a5c6
taffy <taffy@zhihu.com>
2016-02-22 14:00:52 +0800

    添加注释

--- ProgressIndicatorView.h
+++ ProgressIndicatorView.h
@@ -12,7 +12,21 @@ typedef void(^CompletionProgressBlock)();
 
 @interface ProgressIndicatorView : UIView
 
+/**
+ *  添加进度条到 view 上
+ *
+ *  @param inView 进度条始终显示在 inView 的顶端
+ */
 - (void) showProgressAddToView: (UIView *)inView;
-- (void) finishedProgress:(BOOL)finished completionProgress: (CompletionProgressBlock)completionProgress;
+
+
+/**
+ *  事件处理完要此方法关掉进度条
+ *
+ *  @param finished
+ *  @completionProgress
+ */
+- (void) finishedProgress:(BOOL)finished
+       completionProgress: (CompletionProgressBlock)completionProgress;
 
 @end
